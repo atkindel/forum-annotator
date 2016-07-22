c0397bda7dc8c8067a8275a6ec8f8061157895eb
Alex Kindel <alextkindel@gmail.com>
2016-07-22 10:37:28 -0500

    Adjust CLI

--- annotator.py
+++ annotator.py
@@ -6,7 +6,7 @@
 
 import os
 
-from flask import Flask, g, render_template, request, flash, url_for, redirect, session
+from flask import Flask, g, render_template, request, url_for, redirect, session
 from werkzeug import generate_password_hash, check_password_hash
 import sqlite3
 
@@ -14,7 +14,7 @@ import sqlite3
 # Configuration
 DATABASE = "data/annotator.db"
 SECRET_KEY = "DEBUG"
-THREADS = "data/tenthreads.csv"
+THREADS = "data/threads.csv"
 
 # Create application container
 app = Flask(__name__)
@@ -29,14 +29,14 @@ def open_db():
 		g.sqlite_db.row_factory = sqlite3.Row  # treat db rows as dicts
 	return g.sqlite_db
 
-@app.cli.command('initdb')
+@app.cli.command('build')
 def build_db():
 	db = open_db()
 	with open('sql/schema.sql') as f:
 		db.executescript(f.read())
 	db.commit()
-	
-@app.cli.command('loaddb')
+
+@app.cli.command('load')
 def load_db():
 	db = open_db()
 	with open(app.config['THREADS']) as t:
@@ -56,7 +56,7 @@ def set_user():
 	if 'user_id' in session:
 		db = open_db()
 		g.user = db.execute("SELECT * FROM users WHERE id = ?", [session['user_id']]).fetchone()
-		
+	
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 	if g.user:
@@ -72,34 +72,38 @@ def login():
 			session['user_id'] = user['id']
 			return redirect(url_for('index'))
 	return render_template('login.html')
-		
+
+@app.route('/logout')
+def logout():
+	session.pop('user_id', None)
+	return redirect(url_for('index'))
+	
 @app.route('/users')
 def users():
 	db = open_db()
-	users = db.execute('select id, username, first_name, last_name from users').fetchall()
+	users = db.execute('select id, username, first_name, last_name, superuser from users').fetchall()
 	return render_template('users.html', users=users)
 
 @app.route('/admin', methods=['GET', 'POST'])
 def admin():
 	if request.method == 'POST':
 		db = open_db()
+		su = (request.form.get('superuser') == 'on')
 		db.execute("insert into users(username, first_name, last_name, email, pass_hash, superuser) values (?,?,?,?,?,?)",
-			  [request.form['username'], request.form['first_name'], request.form['last_name'], request.form['email'], generate_password_hash(request.form['password']), request.form['superuser']])
+			  [request.form['username'], request.form['first_name'], request.form['last_name'], request.form['email'], generate_password_hash(request.form['password']), su])
 		db.commit()
 		return redirect(url_for('users'))
 	return render_template('admin.html')
 
 
-## App functionality
+## Annotator administration
 
-@app.route('/')
-def index():
-	return render_template('index.html')
-	
 @app.route('/assign', methods=['GET', 'POST'])
 def assign():
 	if not g.user:
 		return redirect(url_for('login'))
+	if not g.user['superuser']:
+		return redirect(url_for('users'))
 	db = open_db()
 	if request.method == 'POST':
 		pass  # TODO: Assign threads to users by checking boxes in a table
@@ -107,6 +111,12 @@ def assign():
 		users = db.execute("SELECT * FROM users ORDER BY id").fetchall()
 		return render_template('assignments.html', users=users)
 
+
+## Main page
+
+@app.route('/')
+def index():
+	return render_template('index.html')
 
 
 if __name__ == "__main__":
