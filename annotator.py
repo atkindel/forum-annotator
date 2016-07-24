15f63e1c58a43fa5d80bff875c8b40cbfb5a301f
Alex Kindel <alextkindel@gmail.com>
2016-07-24 16:22:52 -0400

    Enable assigner view for admin users

--- annotator.py
+++ annotator.py
@@ -4,8 +4,10 @@
 # Author: Alex Kindel
 # Date: 19 July 2016
 
-import os
+from csv import DictReader
 from functools import wraps
+import time
+
 from flask import Flask, g, render_template, request, url_for, redirect, session
 from werkzeug import generate_password_hash, check_password_hash
 import sqlite3
@@ -20,6 +22,12 @@ THREADS = "data/threads.csv"
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+
+## Helper methods
+
+def to_epoch(timestamp):
+	return str(int(time.mktime(time.strptime(timestamp, '%Y-%m-%d %H:%M:%S'))))
+	
 
 ## Database management
 
@@ -40,7 +48,22 @@ def build_db():
 def load_db():
 	db = open_db()
 	with open(app.config['THREADS']) as t:
-		pass  # TODO: load thread data
+		rows = DictReader(t)
+		rowct = 0
+		for row in rows:
+			row['body'] = row['body'].replace('"','""')
+			for key in row.keys():
+				if key not in ['created_at', 'updated_at', 'level', 'comment_count', 'author_id', 'finished']:
+					row[key] = '"%s"' % row[key]
+				elif row[key] in ['NA', '0']:
+ 					row[key] = str(0)
+			row['created_at'] = to_epoch(row['created_at'])
+			row['updated_at'] = to_epoch(row['updated_at'])
+			query = "INSERT INTO threads(%s) VALUES (%s)" % (','.join(row.keys()), ','.join(row.values()))
+			db.execute(query)
+			db.commit()
+			rowct += 1
+		print "Loaded %d forum posts to annotator." % rowct
 
 @app.teardown_appcontext
 def close_db(error):
@@ -102,7 +125,6 @@ def logout():
 	return redirect(url_for('index'))
 
 @app.route('/admin', methods=['GET', 'POST'])
-@superuser_required
 def admin():
 	'''Logic for user admin page'''
 	if request.method == 'POST':
@@ -119,25 +141,44 @@ def admin():
 
 ## Annotator administration
 
+def assigned(thread_id, user_id):
+	db = open_db()
+	assns = db.execute("SELECT * FROM assignments WHERE thread_id = '%s' AND user_id = %d" % (thread_id, int(user_id))).fetchall()
+	return bool(assns)
+
 @app.context_processor
 def assignment_processor():
 	'''Template utility function: is thread_id assigned to user_id?'''
-	def assigned(thread_id, user_id):
+	def fn(thread_id, user_id):
+		return assigned(thread_id, user_id)
+	return dict(assigned=fn)
+	
+@app.context_processor
+def done_processor():
+	''' Template utility function: how many posts in thread X has user Y coded?'''
+	def done(thread_id, user_id):
 		db = open_db()
-		assns = db.execute("SELECT * FROM assignments WHERE thread_id = ? AND user_id = ?", [thread_id, user_id]).fetchall()
-		return "on" if assns else "off"
-	return dict(assigned=assigned)
-		
+		ct = db.execute("SELECT done FROM assignments WHERE thread_id = '%s' AND user_id = %d" % (thread_id, int(user_id))).fetchone()[0]
+		total = db.execute("SELECT count(*) FROM threads WHERE comment_thread_id = '%s'" % thread_id).fetchone()[0]
+		return "%d/%d" % (ct, total)
+	return dict(done=done)
+
 @app.route('/assign', methods=['GET', 'POST'])
-@superuser_required
 def assign():
 	'''Logic for thread assigner'''
 	db = open_db()
+	threads = db.execute("SELECT mongoid, title FROM threads WHERE level = 1").fetchall()
+	users = db.execute("SELECT * FROM users ORDER BY id").fetchall()
 	if request.method == 'POST':
-		pass  # TODO: Assign threads to users by checking boxes in a table
-	else:
-		users = db.execute("SELECT * FROM users ORDER BY id").fetchall()
-		return render_template('assignments.html', users=users)
+		for key in request.form.keys():
+			ids = eval(key)
+			value = request.form[key]
+			thread = ids['thread']
+			user = ids['user']
+			if value == 'on' and not assigned(thread, user):
+				db.execute("INSERT INTO assignments(thread_id, user_id, next_post, finished) VALUES (?,?,?,?)", [thread, user, thread, 0])
+				db.commit()
+	return render_template('assignments.html', users=users, threads=threads)
 
 
 ## Main page
