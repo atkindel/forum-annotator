ab47b774442f54935fe4ed5dd814f9fc83cdc178
Alex Kindel <alextkindel@gmail.com>
2016-08-28 15:23:07 -0700

    Adjust schema to MySQL requirements

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,61 +1,70 @@
+-- Initialize database --
+
+CREATE DATABASE IF NOT EXISTS ForumAnnotator;
+USE ForumAnnotator;
+
 -- Table definitions --
 
 DROP TABLE IF EXISTS `assignments`;
 CREATE TABLE `assignments` (
-    user_id integer not null,
-    thread_id string not null,
-    next_post integer default null,
-    done integer default 0,
-    finished boolean not null
+    assn_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    user_id INTEGER NOT NULL,
+    thread_id TEXT NOT NULL,
+    next_post INTEGER DEFAULT NULL,
+    done INTEGER DEFAULT 0,
+    finished BOOLEAN NOT NULL
 );
 
 DROP TABLE IF EXISTS `threads`;
 CREATE TABLE `threads` (
-    id integer primary key autoincrement,
-    author_id integer not null,
-    author_username text not null,
-    X_type text not null,
-    title text not null,
-    body longtext not null,
-    thread_type text not null,
-    commentable_id text not null,
-    comment_count integer not null,
-    pinned boolean,
-    created_at integer not null,
-    updated_at integer not null,
-    anonymous boolean,
-    parent_ids text,
-    comment_thread_id integer not null,
-    mongoid string not null,
-    level integer not null,
-    finished boolean not null
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    author_id INTEGER NOT NULL,
+    author_username TEXT NOT NULL,
+    X_type TEXT NOT NULL,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    thread_type TEXT NOT NULL,
+    commentable_id TEXT NOT NULL,
+    comment_count INTEGER NOT NULL,
+    pinned BOOLEAN,
+    created_at INTEGER NOT NULL,
+    updated_at INTEGER NOT NULL,
+    anonymous BOOLEAN,
+    parent_ids TEXT,
+    comment_thread_id INTEGER NOT NULL,
+    mongoid TEXT NOT NULL,
+    level INTEGER NOT NULL,
+    finished BOOLEAN NOT NULL
 );
 
 DROP TABLE IF EXISTS `codes`;
 CREATE TABLE `codes` (
-    user_id integer not null,
-    post_id string not null,
-    comment longtext
+    user_id INTEGER NOT NULL,
+    post_id TEXT NOT NULL,
+    comment TEXT
 );
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-    id integer primary key autoincrement,
-    username text not null,
-    first_name text not null,
-    last_name text not null,
-    email text not null,
-    pass_hash text not null,
-    superuser boolean not null
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    username TEXT NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    pass_hash TEXT NOT NULL,
+    superuser BOOLEAN NOT NULL
 );
 
 -- Triggers --
 
-CREATE TRIGGER assignments_default_first_post
+DROP TRIGGER IF EXISTS `assignments_default_first_post`;
+DELIMITER //
+CREATE TRIGGER `assignments_default_first_post`
 AFTER INSERT ON assignments
-FOR EACH ROW WHEN NEW.next_post IS NULL
+FOR EACH ROW #WHEN NEW.next_post IS NULL
 BEGIN
     UPDATE assignments
     SET next_post = NEW.thread_id
-    WHERE rowid = NEW.rowid;
-END;
+    WHERE assn_id = NEW.assn_id;
+END//
+DELIMITER ;
